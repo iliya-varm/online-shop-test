6ca79f31111a9174b6898f171ffeba49db327008
iliya-varm <iliyavarnaserim@gmail.com>
2024-03-11 21:34:46 +0330

    text

---
+++ blueprints/general.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+
+app =  Blueprint("general", __name__)
+
+
+@app.route('/')
+def main():  # put application's code here
+    return 'this is main page'
+
+
+@app.route('/about')
+def about():  # put application's code here
+    return 'about us'
