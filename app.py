3af1b3ba461a428de7b7b6e353f99534a4b2c1c7
iliya-varm <iliyavarnaserim@gmail.com>
2024-03-12 01:40:14 +0330

    admin login and dashboard

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.register_blueprint(admin)
 app.register_blueprint(user)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = config.SQLALCHEMY_DATABASE_URI
-
+app.config['SECRET_KEY'] = config.SECRET_KEY
 extentions.db.init_app(app)
 
 with app.app_context():
--- config.py
+++ config.py
@@ -1,1 +1,6 @@
 SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+
+ADMIN_USERNAME = "iliya1387m"
+ADMIN_PASSWORD = "varnaseri1387m"
+
+SECRET_KEY = "asdfghjklpoiuytrewqzxcvbnmytrewsxcvbnjm"
